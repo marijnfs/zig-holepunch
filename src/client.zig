a6e65913c4e026f3d1f6c07be9da1f4b597ecc5a
Marijn Stollenga <stollenga@imfusion.de>
2021-11-02 22:12:57 +0100

    simple holepunch udp only

--- src/client.zig
+++ src/client.zig
@@ -40,26 +40,43 @@ pub fn main() anyerror!void {
     _ = try os.sendto(sockfd, &buf, 0, &dst_address.any, @sizeOf(std.net.Address));
 
     const read = try os.recv(sockfd, &buf, 0);
-
     if (read != 0) { //we will connect
         std.time.sleep(1000000000);
+
+        // Read the ip
         var ip_buf = buf[0..read];
         const sep = find(ip_buf, ':');
         const ip_addr_str = ip_buf[0..sep];
         const port = try std.fmt.parseInt(u16, buf[sep + 1 .. read], 0);
-
         const target_addr = try std.net.Address.parseIp(ip_addr_str, port);
-        std.log.info("target addr: {s}", .{target_addr});
 
-        var stream_connection = try net.tcpConnectToAddress(target_addr);
-        _ = try stream_connection.write("test");
+        _ = try os.sendto(sockfd, "Test", 0, &target_addr.any, @sizeOf(std.net.Address));
+
+        std.log.info("target addr: {s}", .{target_addr});
     } else { //we will receive
-        var stream_server = std.net.StreamServer.init(net.StreamServer.Options{ .reuse_address = true });
-        try stream_server.listen(address);
-        var stream_connection = try stream_server.accept();
-        var len = try stream_connection.stream.read(&buf);
-        std.log.info("got tcp msg: {s}", .{buf[0..len]});
+        const read2 = try os.recv(sockfd, &buf, 0);
+        std.log.info("Got: {s}", .{buf[0..read2]});
     }
+
+    // if (read != 0) { //we will connect
+    //     std.time.sleep(1000000000);
+    //     var ip_buf = buf[0..read];
+    //     const sep = find(ip_buf, ':');
+    //     const ip_addr_str = ip_buf[0..sep];
+    //     const port = try std.fmt.parseInt(u16, buf[sep + 1 .. read], 0);
+
+    //     const target_addr = try std.net.Address.parseIp(ip_addr_str, port);
+    //     std.log.info("target addr: {s}", .{target_addr});
+
+    //     var stream_connection = try net.tcpConnectToAddress(target_addr);
+    //     _ = try stream_connection.write("test");
+    // } else { //we will receive
+    //     var stream_server = std.net.StreamServer.init(net.StreamServer.Options{ .reuse_address = true });
+    //     try stream_server.listen(address);
+    //     var stream_connection = try stream_server.accept();
+    //     var len = try stream_connection.stream.read(&buf);
+    //     std.log.info("got tcp msg: {s}", .{buf[0..len]});
+    // }
 
     // _ = try os.sendto(sockfd, &buf, 0, &target_addr.any, @sizeOf(std.net.Address));
 
